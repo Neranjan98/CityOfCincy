b576eb7a12b486f072d17b102ba4ef61948d4da2
Neranjan R S <neranjansugumar1998@gmail.com>
2023-10-20 17:53:01 -0400

    Renamed Application - Initial Check in

--- CityOfCincy/Pages/Error.cshtml.cs
+++ CityOfCincy/Pages/Error.cshtml.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Diagnostics;
 
-namespace BookstoreApplication.Pages
+namespace CityOfCincy.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     [IgnoreAntiforgeryToken]
