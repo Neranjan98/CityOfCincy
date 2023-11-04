ea0fc4ce068c33f1ba4dd48d54baf17357c7ba4c
patilhrishi18 <patilhrishikesh18@gmail.com>
2023-11-04 14:23:05 -0400

    Added Generic API Requests.

---
+++ CityOfCincy/Utilities/APIRequestHandler.cs
@@ -0,0 +1,34 @@
+﻿using RestSharp;
+
+namespace CityOfCincy.Utilities
+{
+    public static class APIRequestHandler
+    {
+        public static async Task<string> MakeGETRestRequest(RestRequest restRequest)
+        {
+            RestClient restClient = new();
+            RestResponse restResponse = new();
+            restResponse = await restClient.GetAsync(restRequest);
+            string returnString;
+            if (restResponse.IsSuccessful)
+            {
+                if (!String.IsNullOrWhiteSpace(restResponse.Content))
+                {
+                    returnString = restResponse.Content;
+                }
+                else
+                {
+                    returnString = String.Empty;
+                }
+            }
+            else
+            {
+                returnString = String.Empty;
+            }
+            restClient.Dispose();
+
+            return returnString;
+        }
+
+    }
+}
---
+++ CityOfCincy/Utilities/IDataResolver.cs
@@ -0,0 +1,13 @@
+﻿using CityOfCincy.Models;
+
+namespace CityOfCincy.Utilities
+{
+    public interface IDataResolver
+    {
+        Task<List<EmployeeSalaryResponse>?> GetCincinnatiSalaryInfo();
+        Task<List<EmployeeSalaryResponse>?> GetSalaryDataWithQueryParemeter(string queryParam, string value);
+        Task<List<EmployeeSalaryScheduleResponse>?> GetCincinnatiSalaryScheduleInfo();
+        Task<List<DepartmentInfoResponse>?> GetCincinnatiDepartmentInfo();
+        Task<List<DepartmentInfoResponse>?> GetIndividualCincinnatiDepartmentInfo(string dept_code);
+    }
+}
