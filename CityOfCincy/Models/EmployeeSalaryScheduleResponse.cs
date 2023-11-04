3bd435be79b4897bececdae72091dd419855ed49
Bandhavi Aakasapu <aakasabi@mail.uc.edu>
2023-11-04 13:19:58 -0400

    Adding employee salary schedule response model

---
+++ CityOfCincy/Models/EmployeeSalaryScheduleResponse.cs
@@ -0,0 +1,17 @@
+﻿namespace CityOfCincy.Models
+{
+    public class EmployeeSalaryScheduleResponse
+    {
+
+        public string? description { get; set; }
+        public DateTime? effective_date { get; set; }
+        public string? salary_plan { get; set; }
+        public string? salary_plan_desc { get; set; }
+        public string? grade { get; set; }
+        public string? min_hour { get; set; }
+        public string? max_hour { get; set; }
+        public string? min_annual { get; set; }
+        public string? max_annual { get; set; }
+
+    }
+}
