95595649a5eb818ce5afefa0f25b627838960f4e
Bandhavi Aakasapu <aakasabi@mail.uc.edu>
2023-11-04 13:18:19 -0400

    Adding DepartmentInfo Model

---
+++ CityOfCincy/Models/DepartmentInfoResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace CityOfCincy.Models
+{
+    public class DepartmentInfoResponse
+    {
+
+            public string? department_codes { get; set; }
+            public string? department { get; set; }
+            public string? director { get; set; }
+            public string? department_main_website { get; set; }
+            public string? department_contact_information { get; set; }
+    }
+}
+
